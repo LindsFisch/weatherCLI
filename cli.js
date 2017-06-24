a6ebe50eabcfaadb4d7ba27b596e173211b51a93
Lindsey Fischer <lindsfisch@gmail.com>
2017-06-24 11:29:15 -0500

    updated

--- cli.js
+++ cli.js
@@ -12,18 +12,5 @@ var MyAdmin = new WeatherAdmin();
 if (loginType === "admin") {
     MyAdmin.getData();
 } else {
-    MyAdmin.userSearch(userName, city);
-}
-
-function UserSearch (userName, city) {
-    this.userName = userName;
-    this.city = city;
-}
-
-UserSearch.prototype.getWeather = function () {
-    weather.find({search: city , degreeType: 'F'}, function(err, result) {
-        if(err) console.log(err);
- 
-  console.log(JSON.stringify(result, null, 2));
-});
+    MyAdmin.newUserSearch(userName, city);
 }
