a2f7b3a23dd93771afee06decc4616bace9cefdf
Lindsey Fischer <lindsfisch@gmail.com>
2017-06-24 10:54:06 -0500

    updated file

--- cli.js
+++ cli.js
@@ -1,2 +1,15 @@
 var weather = require('weather-js');
 
+var loginType = process.argv[2];
+
+var userName = process.argv[3];
+
+var city = process.argv[4];
+
+var MyAdmin = new WeatherAdmin();
+
+if (loginType === "admin") {
+    MyAdmin.getData();
+} else {
+    MyAdmin.newSearch(userName, city);
+}
